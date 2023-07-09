0ebb6e8e2b1c2c946181870cadd0d77164cfa1d2
Antonenko <42521426+167899@users.noreply.github.com>
2023-07-09 14:41:32 +0300

    create app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Button from "./components/button";
+
+function App() {
+  return (
+    <div className="App">
+      Hello world!
+      <Button text="1" />
+      <Button text="2" />
+      <Button text="3" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/button.jsx
@@ -0,0 +1,7 @@
+function Button(props) {
+  return (
+    <button type="button">button { props.text }</button>
+  );
+}
+
+export default Button;
